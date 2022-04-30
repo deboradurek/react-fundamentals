e75a0e15874b0fd93a666c89204e87aa1ae58677
Debora Durek <debora.durek@gmail.com>
2022-04-29 19:17:54 -0600

    Forms [Extra Credit] Validate lower-case

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -5,6 +5,7 @@ import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
   const userNameInputRef = React.useRef()
+  const [error, setError] = React.useState(null)
 
   const handleSubmit = event => {
     event.preventDefault()
@@ -12,13 +13,27 @@ function UsernameForm({onSubmitUsername}) {
     onSubmitUsername(userName)
   }
 
+  const handleChange = event => {
+    const {value} = event.target
+    const isLowerCase = value === value.toLowerCase()
+    setError(!isLowerCase && 'Username must be lower case.')
+  }
+
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="textID">Username:</label>
-        <input ref={userNameInputRef} id="textID" type="text" />
+        <input
+          ref={userNameInputRef}
+          id="textID"
+          type="text"
+          onChange={handleChange}
+        />
       </div>
-      <button type="submit">Submit</button>
+      <button type="submit" disabled={Boolean(error)}>
+        Submit
+      </button>
+      <div style={{color: 'red'}}>{error}</div>
     </form>
   )
 }
